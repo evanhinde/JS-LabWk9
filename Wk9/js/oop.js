e05548148b9b488ca410b0077a23a62be78aede0
evanhinde <113151227+evanhinde@users.noreply.github.com>
2023-11-05 18:44:35 -0500

    Add files via upload

---
+++ Wk9/js/oop.js
@@ -0,0 +1,89 @@
+const output = document.getElementById('output');
+const output2 = document.getElementById('output2');
+
+/* STEP 1a: Create a new object using a regular function */
+function createNewPerson(name) {
+    var obj = {};
+    obj.name = name;
+    obj.greetings = function() {
+        alert(`Hello! I am ${this.name}`);
+    }
+    return obj;
+}
+
+/* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
+var person1 = createNewPerson("Person1");
+var person2 = createNewPerson("Person2");
+/* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
+rewrite the above function, without returning anything. Capitalize the name of the function. */
+function Person(firstName, lastName, age, gender, interests) {
+    this.name =  {
+        "firstName": firstName,
+        "lastName": lastName,
+    },
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function () {
+        return `The interests of ${this.name.firstName} ${this.name.lastName} of age ${this.age} of gender ${this.gender} are ${this.interests}`;
+    }
+}
+/* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
+and again invoking the .greeting() method for each person */
+var p2 = new Person("p2", "p2", 20, "Male", ["baseball", "golfing", "fishing"]);
+/* STEP 3a: Build the complete constructor for the object Person (comment out the above function first).
+ Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
+
+/* STEP 3b: Instantiate a new Person based on the above constructor */
+
+/* STEP 3c: Attempt to access the various properties of person1 using the console. */
+// person1['age']
+// person1.interests[1]
+// person1.bio()
+
+/* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
+var car = new Object();
+/* STEP 4b: Once 'car' is created, add various properties and methods… */
+car.brand = "Honda";
+car.color = "White";
+car.price = 50;
+/* STEP 4c: Change some of the properties of 'car' in the console, then invoke the car.fun() function */
+
+/* STEP 5a: Yet another approach is to use the create() method. 
+Let's see how the above car object can be used to create another object */
+var anotherCar = Object.create(car);
+/* STEP 5b: Output to the paragraph anotherCar.brand - you will see that it has retained the properties of the original object. */
+console.log(anotherCar.brand);
+// That's it! Now on to Lab 3...
+// Create a JavaScript function that permits the creation of a hamburger object
+// Consider that a hamburger has many options – that might include
+/*
+    different types of buns,
+    vegetable garnishes, 
+    assortments of cheeses, 
+    specialty sauces, 
+    different meat patties(beef, chicken, vegetarian), 
+    single, double, or triple patty, 
+    Pickles, hot peppers, olives etc.
+*/
+// add some extra if you want
+// Make sure that the function allows for the output of a sentence that describes the hamburger, too
+
+function Hamburger(bun, vegetables, cheese, sauces, meat, pattyNo) {
+    this.bun = bun;
+    this.vegetables = vegetables;
+    this.cheese = cheese;
+    this.sauces = sauces;
+    this.meat = {
+        "meatType": meat,
+        "pattyNo": pattyNo,
+    },
+    this.burger = function () {
+        return `Your burger is on ${this.bun} bun, with these vegetables: ${this.vegetables}, with ${this.cheese} cheese, these sauces: ${this.sauces}, and has ${this.meat.pattyNo} pattie(s) made of ${this.meat.meatType}`;
+    }
+}
+
+var burger1 = new Hamburger("White", ["Lettuce", "Tomato", "Onion"], "Cheddar", ["Ketchup", "Mayo"], "Beef", "3");
+
+output.textContent = burger1.burger();
+// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
